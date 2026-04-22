21004e283ec14c14a4df78746bb1784f2892d258
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-04-23 00:38:27 +0500

    Add loading skeleton & change stale time

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -10,6 +10,8 @@ import PostItem from "@/components/post/PostItem";
 import { ErrorState } from "@/components/states/ErrorState";
 import SegmentedTabs from "@/components/common/SegmentedTabs";
 import { useState } from "react";
+import PostsSkeleton from "@/components/skeleton/PostsSkeleton";
+import { EmptyState } from "@/components/states/EmptyState";
 
 type Tabs = "all" | "free" | "paid";
 
@@ -24,27 +26,29 @@ export default function Feed() {
     isFetchingNextPage,
     isRefetching,
     isError,
+    isFetching,
     refetch,
   } = usePosts(activeTab);
 
   const queryClient = useQueryClient();
+  const skeletons = [1, 2, 3, 4, 5];
 
   if (isError) {
     return <ErrorState onRetry={refetch} />;
   }
-
-  if (isLoading)
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
 
   const posts = data?.pages.flatMap((post) => post.posts) ?? [];
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <SegmentedTabs value={activeTab} onChange={setActiveTab} />
+      {isLoading ||
+        (!posts &&
+          skeletons.map((_, index) => (
+            <View key={index}>
+              <PostsSkeleton />
+            </View>
+          )))}
       <FlatList
         style={{ backgroundColor: colors.background }}
         data={posts}
@@ -61,6 +65,7 @@ export default function Feed() {
         onRefresh={() => {
           queryClient.invalidateQueries({ queryKey: ["posts"] });
         }}
+        ListEmptyComponent={isLoading ? <PostsSkeleton /> : <EmptyState />}
         ListFooterComponent={isFetchingNextPage ? <ActivityIndicator /> : null}
       />
     </SafeAreaView>
